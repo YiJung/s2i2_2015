81ccdc8b0cf1586303834f2e5137749a4c8806b5
Alice Walker <awalker@chem.wayne.edu>
2015-06-23 09:45:29 -0400

    Testing temperature program; not working as intended.

---
+++ src/temperature.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+''' This is a python module that converts temperatures. '''
+
+#The following function converts from Farenheit to Kelvin.
+def f_to_k(tempi):
+    converted = ((temp - 32) * (5 / 9) + 273.15
+    return converted
+
+print(f_to_k(32))
+print(f_to_k(100))
