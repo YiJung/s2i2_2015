85fc6e3061adec25ffa36c5d252365bd2843914e
Alice Walker <awalker@chem.wayne.edu>
2015-06-23 10:00:05 -0400

    Added temperature conversions for celsius and kelvin. Fixed errors.

--- src/temperature.py
+++ src/temperature.py
@@ -4,8 +4,18 @@
 
 #The following function converts from Farenheit to Kelvin.
 def f_to_k(tempi):
-    converted = ((tempi - 32.0) * (5.0 / 9.0) + 273.15
+    converted = ((tempi - 32.0) * (5.0 / 9.0)) + 273.15
     return converted
+
+def k_to_c(tempi):
+    return tempi - 273.15
+
+def f_to_c(tempi):
+    temp_k = f_to_k(tempi)
+    result = k_to_c(temp_k)
+    return result
 
 print(f_to_k(32))
 print(f_to_k(100))
+print(k_to_c(273.15))
+print(f_to_c(32))
